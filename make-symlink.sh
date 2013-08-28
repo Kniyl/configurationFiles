b802a20af1e404b0bd8703865b8228729001d6e4
Mathias Ettinger <mathias.ettinger@imag.fr>
2013-08-28 13:49:21 +0200

    Gtk theme modification

--- make-symlink.sh
+++ make-symlink.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-ln -s ~/.config/configurationFiles/vimrc ~/.vimrc
-ln -s ~/.config/configurationFiles/latexmkrc ~/.latexmkrc
+
+confDir="~/.config/configurationFiles"
+
+cp $confDir/bashrc ~/.bashrc
+ln -s $confDir/vimrc ~/.vimrc
+ln -s $confDir/latexmkrc ~/.latexmkrc
+
 mkdir -p ~/PkgBuilding/
-ln -s ~/.config/configurationFiles/yaourtrc ~/.yaourtrc
-cp bashrc ~/.bashrc
+ln -s $confDir/yaourtrc ~/.yaourtrc
+
+ln -s $confDir/ArchCursorBlue ~/.icons/ArchCursorBlue
+ln -s $confDir/OxygenRefit2-black-version ~/.icons/OxygenRefit2-black-version
+ln -s $confDir/gtkrc-2.0 ~/.gtkrc-2.0
