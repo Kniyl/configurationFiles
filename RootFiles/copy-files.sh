4590cf151434de226e10ad912413464c9e80a1c5
Kniyl <mathias.ettinger@gmail.com>
2013-08-30 09:29:30 +0200

    Adding keyboard configuration under Xorg

--- RootFiles/copy-files.sh
+++ RootFiles/copy-files.sh
@@ -5,3 +5,4 @@ cp modprobe.conf /etc/modprobe.d/modprobe.conf
 cp slim.conf /etc/slim.conf
 cat vimrc >> /etc/vimrc
 cp vimrc.bepo /etc/vimrc.bepo
+cp keyboard.conf /etc/X11/xorg.conf.d/20-keyboard.conf
