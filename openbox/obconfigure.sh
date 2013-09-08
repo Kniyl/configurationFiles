bd8810205eb50d43d5c6bc7d7bafefb57091881c
Mathias Ettinger <mathias.ettinger@gmail.com>
2013-09-08 22:40:46 +0200

    Ajout polices openbox

--- openbox/obconfigure.sh
+++ openbox/obconfigure.sh
@@ -2,12 +2,12 @@
 
 confDir="$HOME/.config/configurationFiles/openbox"
 
-yaourt -S --needed openbox openbox-themes feh lalcal xcompmgr stalonetray obfilebrowser mimeo pcmanfm gnome-menus2 lxpolkit
+yaourt -S --needed openbox openbox-themes feh lalcal xcompmgr stalonetray obfilebrowser mimeo pcmanfm gnome-menus2 lxpolkit ttf-tlwg
 mkdir -p ~/.config/openbox/Wallpapers/
 cp $confDir/Wallpapers/* ~/.config/openbox/Wallpapers/
 cp $confDir/rc.xml $confDir/menu.xml $confDir/autostart.sh $confDir/background $confDir/toggle_display ~/.config/openbox/
 
-yaourt -S xfce4-terminal ttf-bitstream-vera
+yaourt -S --needed xfce4-terminal ttf-bitstream-vera
 mkdir -p ~/.config/Terminal/
 cp $confDir/terminalrc ~/.config/Terminal/
 
