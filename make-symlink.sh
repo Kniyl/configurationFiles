a4d1274d80cb953016ccabbf12ebf439f6f7e548
Kniyl <mathias.ettinger@gmail.com>
2013-09-08 21:31:23 +0200

    Modification de la gestion des paquets à installer

--- make-symlink.sh
+++ make-symlink.sh
@@ -8,6 +8,7 @@ ln -s $confDir/latexmkrc ~/.latexmkrc
 mkdir -p ~/PkgBuilding/
 ln -s $confDir/yaourtrc ~/.yaourtrc
 
+yaourt -S gtk-theme-murrine-arch ttf-cheapskate ttf-dejavu
 ln -s $confDir/ArchCursorBlue ~/.icons/ArchCursorBlue
 ln -s $confDir/OxygenRefit2-black-version ~/.icons/OxygenRefit2-black-version
 ln -s $confDir/gtkrc-2.0 ~/.gtkrc-2.0
