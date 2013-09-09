dec4a1ee83d7fc02dc6df01965e239b44c5015b6
Mathias Ettinger <mathias.ettinger@imag.fr>
2013-09-09 15:27:58 +0200

    Corrections terminal

--- make-symlink.sh
+++ make-symlink.sh
@@ -12,7 +12,3 @@ yaourt -S --needed gtk-theme-murrine-arch ttf-cheapskate ttf-dejavu
 cp -r $confDir/ArchCursorBlue ~/.icons/ArchCursorBlue
 cp -r $confDir/OxygenRefit2-black-version ~/.icons/OxygenRefit2-black-version
 ln -s $confDir/gtkrc-2.0 ~/.gtkrc-2.0
-
-mkdir -p $confDir/Terminal/
-ln -s $confDir/terminalrc $confDir/Terminal/terminalrc
-
--- openbox/obconfigure.sh
+++ openbox/obconfigure.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.config/openbox/Wallpapers/
 cp $confDir/Wallpapers/* ~/.config/openbox/Wallpapers/
 cp $confDir/rc.xml $confDir/menu.xml $confDir/autostart.sh $confDir/background $confDir/toggle_display ~/.config/openbox/
 
-yaourt -S --needed xfce4-terminal ttf-bitstream-vera
+yaourt -S --needed xfce4-terminal ttf-bitstream-vera ttf-dejavu
 mkdir -p ~/.config/Terminal/
 cp $confDir/terminalrc ~/.config/Terminal/
 
