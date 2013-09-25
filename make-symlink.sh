927eebae2ed2ed53b7652668161e37062849b164
Mathias Ettinger <mathias.ettinger@gmail.com>
2013-09-25 19:28:34 +0200

    Better bash syntax

--- make-symlink.sh
+++ make-symlink.sh
@@ -5,8 +5,7 @@ confDir="$HOME/.config/configurationFiles"
 cp $confDir/bashrc ~/.bashrc
 ln -s $confDir/latexmkrc ~/.latexmkrc
 
-mkdir -p ~/PkgBuilding/yaourt.sources
-mkdir -p ~/PkgBuilding/yaourt.devel
+mkdir -p ~/PkgBuilding/yaourt.{sources,devel}
 ln -s $confDir/yaourtrc ~/.yaourtrc
 
 yaourt -S --needed gtk-theme-murrine-arch ttf-cheapskate ttf-dejavu
--- openbox/obconfigure.sh
+++ openbox/obconfigure.sh
@@ -5,12 +5,12 @@ confDir="$HOME/.config/configurationFiles/openbox"
 yaourt -S --needed openbox openbox-themes feh lalcal xcompmgr stalonetray obfilebrowser mimeo pcmanfm gnome-menus2 lxpolkit ttf-tlwg scrot
 mkdir -p ~/.config/openbox/Wallpapers/
 cp $confDir/Wallpapers/* ~/.config/openbox/Wallpapers/
-cp $confDir/rc.xml $confDir/menu.xml $confDir/autostart.sh $confDir/background $confDir/toggle_display ~/.config/openbox/
+cp $confDir/{{rc,menu}.xml,autostart.sh,background,toggle_display} ~/.config/openbox/
 ln -s $confDir/stalonetrayrc ~/.stalonetrayrc
 
 yaourt -S --needed xfce4-terminal ttf-bitstream-vera ttf-dejavu
 mkdir -p ~/.config/Terminal/
-cp $confDir/terminalrc ~/.config/Terminal/
+ln -s $confDir/terminalrc ~/.config/Terminal/
 
 sudo cp genmenu genplaces pyprocesses /usr/bin
 
