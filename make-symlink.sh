6a13bf3d4e8dfce07e70bbfd626915a2bd381ac1
Kniyl <mathias.ettinger@gmail.com>
2013-09-27 22:07:33 +0200

    Fix non-existent path for copy

--- make-symlink.sh
+++ make-symlink.sh
@@ -8,7 +8,9 @@ ln -s $confDir/latexmkrc ~/.latexmkrc
 mkdir -p ~/PkgBuilding/yaourt.{sources,devel}
 ln -s $confDir/yaourtrc ~/.yaourtrc
 
+echo "Yaourt will install gtk themes capabilities"
 yaourt -S --needed gtk-theme-murrine-arch ttf-cheapskate ttf-dejavu
+mkdir -p ~/.icons/
 cp -r $confDir/ArchCursorBlue ~/.icons/ArchCursorBlue
 cp -r $confDir/OxygenRefit2-black-version ~/.icons/OxygenRefit2-black-version
 ln -s $confDir/gtkrc-2.0 ~/.gtkrc-2.0
